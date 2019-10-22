2c3d212d8b2e31e2e6da8ceb5de296d1b499e2ae
tommie1989 <hayes2019913@hotmail.com>
2019-10-21 22:08:05 -0700

    updage model.py

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,15 @@ from wagtail.search import index
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [
-        FieldPanel('intro', classname="full")
-    ]
+    # content_panels = Page.content_panels + [
+    #     FieldPanel('intro', classname="full")
+    # ]
+    def get_content(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by('-first_published_at')
+        context['blogpages'] = blogpages
+        return context
 
 class BlogPage(Page):
     date = models.DateField("Post date")
